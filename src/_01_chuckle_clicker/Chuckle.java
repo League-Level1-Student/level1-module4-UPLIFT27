04f9b92b86a9f52841421091be129533a9ab6ef3
Sashrik Selva <sashrikselva@gmail.com>
2024-02-15 18:28:42 -0800

    continue chuckle

--- src/_01_chuckle_clicker/Chuckle.java
+++ src/_01_chuckle_clicker/Chuckle.java
@@ -1,5 +1,42 @@
 package _01_chuckle_clicker;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class Chuckle {
 
+	JFrame frame = new JFrame();
+	JButton joke = new JButton();
+	JButton punchline = new JButton();
+	
+	
+	
+	joke.addActionListner();
+	punchline.addActionListner();
+	
+	JOptionPane.showMessageDialog(null, "hi");
+	
+	
+	frame.pack();
+
+
+	
+	if(event.getSource() == joke)
+	{
+		System.out.println("hello");
+	}
+	
+	if(event.getSource() == joke)
+	{
+		
+		System.out.println("you got pranked");
+	}
+	
+	static public void main(String[] args) {
+		
+		
+		
+		
+	}
+	
 }
